85e49b131f9fdecac68a035abd9e503e50aee6a5
soorena <soorenazohrabi@gmail.com>
2024-02-27 16:30:24 +0330

    feat(login.html): add login form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,23 @@
+
+const loginButton = document.getElementById("loginButton");
+
 function s() {
-    if(window.innerWidth <= 480) {
+    if (window.innerWidth <= 480) {
         document.getElementById('footerH').className = "col-12 h1 text-center";
         document.querySelector('footer .list-group').style.display = "none";
         document.querySelector('footer .accordion').style.display = "";
-    } else if(window.innerWidth > 480) {
+    } else if (window.innerWidth > 480) {
         document.getElementById('footerH').className = "col-3 h1 text-center";
         document.querySelector('footer .list-group').style.display = "";
         document.querySelector('footer .accordion').style.display = "none";
+    }
+}
+
+function checkAdminUsername() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username.toUpperCase() === "ADMINMOB" && password === "13579A") {
+        console.log(username, password, loginButton);
+        loginButton.setAttribute('href', 'dashboard.html');
     }
 }
